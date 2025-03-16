20215d9eaad363268e09a4e3072496d2e6863b41
Owen <34784541+artneers@users.noreply.github.com>
2025-03-16 14:55:00 +0800

    add GeoQuiz demo

---
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.geoquiz
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+private const val TAG = "QuizViewModel"
+
+class QuizViewModel: ViewModel() {
+    private val questionBank = listOf(
+        Question(R.string.question_china, true),
+        Question(R.string.question_taiyuan, false),
+        Question(R.string.question_xian, true),
+    )
+    var currentIndex = 0
+    var isCheater = false
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].answer
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToText() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+}
